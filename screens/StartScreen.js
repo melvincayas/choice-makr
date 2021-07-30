2102ae728162b3231a13ec4a6b71a17733f6e052
Melvin Cayas <cayasmj@gmail.com>
2021-07-29 22:22:01 -0400

    Created initial StartScreen

---
+++ screens/StartScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+const StartScreen = () => {
+	return (
+		<View>
+			<TextInput />
+			<Text>Enter choices</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default StartScreen;
