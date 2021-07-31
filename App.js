2627d6f3ecfd10940a116e0aecc5a7b3fb04c801
Melvin Cayas <cayasmj@gmail.com>
2021-07-30 22:10:24 -0400

    Added StartScreen

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import StartScreen from "./screens/StartScreen";
 
 import Header from "./components/Header";
 
 export default function App() {
+	const [choices, setChoices] = useState([]);
+
+	const onSubmitChoiceHandler = choice => {
+		setChoices(prevChoices => [...prevChoices, choice]);
+	};
+
 	return (
-		<View style={styles.container}>
+		<View style={styles.appContainer}>
 			<Header />
+			<StartScreen onSubmit={onSubmitChoiceHandler} />
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
+	appContainer: {
 		flex: 1,
 	},
 });
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,15 +1,59 @@
-import React from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
 
-const StartScreen = () => {
+const StartScreen = props => {
+	const [enteredChoice, setEnteredChoice] = useState("");
+
+	const onChangeTextHandler = enteredText => {
+		setEnteredChoice(enteredText);
+	};
+
+	const onPressHandler = () => {
+		props.onSubmit(enteredChoice);
+	};
+
 	return (
-		<View>
-			<TextInput />
-			<Text>Enter choices</Text>
+		<View style={styles.container}>
+			<View style={styles.textInputContainer}>
+				<TextInput
+					style={styles.textInput}
+					value={enteredChoice}
+					onChangeText={onChangeTextHandler}
+					placeholder="Enter choices"
+				/>
+			</View>
+			<View style={styles.button}>
+				<Button title="Enter" color="#87CEEB" onPress={onPressHandler} />
+			</View>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		alignItems: "center",
+	},
+	textInputContainer: {
+		flexDirection: "row",
+		justifyContent: "center",
+		marginVertical: 15,
+	},
+	textInput: {
+		fontSize: 18,
+		borderColor: "black",
+		borderWidth: 1,
+		borderRadius: 10,
+		paddingHorizontal: 10,
+		paddingVertical: 3,
+		width: "80%",
+	},
+	button: {
+		overflow: "hidden",
+		width: "35%",
+		borderRadius: 10,
+		borderColor: "black",
+		borderWidth: 1,
+	},
+});
 
 export default StartScreen;
