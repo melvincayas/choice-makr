143e0b4fe9335d0ceee28feeff6998ccd78c9c1e
Melvin Cayas <cayasmj@gmail.com>
2021-08-04 20:02:02 -0400

    Created constants for Styles and added buttonStyles

---
+++ constants/Styles.js
@@ -0,0 +1,11 @@
+import { Colors } from "./Colors";
+
+export const buttonStyles = {
+	button: {
+		borderColor: Colors.black,
+		borderRadius: 10,
+		borderWidth: 1,
+		overflow: "hidden",
+		width: "30%",
+	},
+};
