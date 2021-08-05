7e4d02d916f80b473f55efdd5c04f9b37c6ceb9a
Melvin Cayas <cayasmj@gmail.com>
2021-08-04 20:01:42 -0400

    Converted App useState to useReducer

--- App.js
+++ App.js
@@ -1,33 +1,65 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/UI/Header";
 import StartScreen from "./screens/StartScreen";
 import NumberOfChoicesScreen from "./screens/NumberOfChoicesScreen";
 
+const initialState = {
+	choices: [],
+	isFinishedEnteringChoices: false,
+};
+
+const appStateReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "ENTERED_CHOICE":
+			return {
+				...state,
+				choices: [...state.choices, action.payload],
+			};
+		case "DELETE_CHOICE":
+			return {
+				...state,
+				choices: state.choices.filter(choice => choice.id !== action.payload),
+			};
+		case "FINISHED_ENTERING":
+			return {
+				...state,
+				isFinishedEnteringChoices: true,
+			};
+		case "RESET":
+			return {
+				choices: [],
+				isFinishedEnteringChoices: false,
+			};
+		default:
+			return initialState;
+	}
+};
+
 export default function App() {
-	const [choices, setChoices] = useState([]);
-	const [isFinishedEnteringChoices, setIsFinishedEnteringChoices] =
-		useState(false);
+	const [appState, dispatch] = useReducer(appStateReducer, initialState);
 
 	const onSubmitChoiceHandler = enteredChoice => {
-		setChoices(prevChoices => [...prevChoices, enteredChoice]);
+		dispatch({ type: "ENTERED_CHOICE", payload: enteredChoice });
 	};
 
 	const onDeleteChoiceHandler = deletedChoice => {
-		setChoices(prevChoices =>
-			prevChoices.filter(choice => choice.id !== deletedChoice)
-		);
+		dispatch({ type: "DELETE_CHOICE", payload: deletedChoice });
 	};
 
 	const onFinishEnteringChoices = () => {
-		setIsFinishedEnteringChoices(prevState => !prevState);
+		dispatch({ type: "FINISHED_ENTERING" });
 	};
 
-	const screenProgression = isFinishedEnteringChoices ? (
-		<NumberOfChoicesScreen />
+	const onResetChoices = () => {
+		dispatch({ type: "RESET" });
+	};
+
+	const screenProgression = appState.isFinishedEnteringChoices ? (
+		<NumberOfChoicesScreen onReset={onResetChoices} />
 	) : (
 		<StartScreen
-			choices={choices}
+			choices={appState.choices}
 			onDelete={onDeleteChoiceHandler}
 			onSubmit={onSubmitChoiceHandler}
 			onFinish={onFinishEnteringChoices}
